f628f130ded9b746402bf5eaa4f772a559f63f03
Christopher Barrett <municipalshock@gmail.com>
2020-12-22 17:33:51 -0800

    assets and bird made

--- flappy.py
+++ flappy.py
@@ -1,7 +1,33 @@
-import pygame
+import pygame, sys
+
+def draw_floor():
+    screen.blit(floor_surface, (floor_x_pos,900))
+    screen.blit(floor_surface, (floor_x_pos + 576,900))
+
+
 
 pygame.init()
 screen = pygame.display.set_mode((576, 1024))
+clock = pygame.time.Clock()
+
+#game vars
+gravity = 0.25
+bird_movement = 0
+
+bg_surface = pygame.image.load('flappy-bird-assets/sprites/background-day.png').convert()
+bg_surface = pygame.transform.scale2x(bg_surface)
+
+floor_surface = pygame.image.load('flappy-bird-assets/sprites/base.png').convert()
+floor_surface = pygame.transform.scale2x(floor_surface)
+floor_x_pos = 0
+
+bird_surface = pygame.image.load('flappy-bird-assets/sprites/bluebird-midflap.png').convert()
+bird_surface = pygame.transform.scale2x(bird_surface)
+bird_rect = bird_surface.get_rect(center = (100, 512))
+
+pipe_surface = pygame.image.load('flappy-bird-assets/sprites/pipe-green.png').convert()
+pipe_surface = pygame.transform.scale2x(pipe_surface)
+
 
 while True:
     #player image
@@ -9,4 +35,20 @@ while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
+            sys.exit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                bird_movement = 0
+                bird_movement -= 12
+    screen.blit(bg_surface, (0,0))
+
+    bird_movement += gravity
+    bird_rect.centery += bird_movement
+    screen.blit(bird_surface, bird_rect)
+    floor_x_pos -= 1
+    draw_floor()
+    if floor_x_pos <= -576:
+        floor_x_pos = 0
+
     pygame.display.update()
+    clock.tick(120)
